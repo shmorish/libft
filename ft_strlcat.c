b1ed59f0a24293c74cd8385a3410db4defbac423
shmorish <01morishi@gmail.com>
2023-05-16 19:40:42 +0900

    strlcat, strlcpy

---
+++ ft_strlcat.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shmorish <shmorish@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/16 19:35:49 by shmorish          #+#    #+#             */
+/*   Updated: 2023/05/16 19:38:58 by shmorish         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stddef.h>
+
+size_t	ft_strlcat(char *dest, const char *src, size_t dstsize)
+{
+	size_t	index;
+	size_t	max_copy;
+	size_t	dest_len;
+	size_t	src_len;
+
+	dest_len = ft_strlen(dest);
+	src_len = ft_strlen(src);
+	if (dstsize <= dest_len)
+		return (dstsize + src_len);
+	max_copy = dstsize - dest_len - 1;
+	index = 0;
+	while (index < max_copy)
+	{
+		if (src[index] == '\0')
+			break ;
+		dest[dest_len + index] = src[index];
+		index++;
+	}
+	dest[dest_len + index] = '\0';
+	return (src_len + dest_len);
+}
---
+++ ft_strlcpy.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shmorish <shmorish@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/16 19:33:01 by shmorish          #+#    #+#             */
+/*   Updated: 2023/05/16 19:39:59 by shmorish         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t	i;
+
+	if (!dst || !src)
+		return (0);
+	if (dstsize == 0)
+		return (ft_strlen(src));
+	i = 0;
+	while (src[i] && i < (dstsize - 1))
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (ft_strlen(src));
+}
