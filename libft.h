877b4a2a0731b306906c83dbd9e50de9fd2ae722
shmorish <01morishi@gmail.com>
2023-05-16 21:49:34 +0900

    few fix

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: shmorish <shmorish@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 16:06:26 by shmorish          #+#    #+#             */
-/*   Updated: 2023/05/16 19:56:02 by shmorish         ###   ########.fr       */
+/*   Updated: 2023/05/16 21:44:22 by shmorish         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,5 +26,10 @@ void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcat(char *dest, const char *src, size_t dstsize);
+int		toupper(int c);
+int		tolower(int c);
+char	*strchr(const char *s, int c);
+char	*strrchr(const char *s, int c);
+int		strncmp(const char *s1, const char *s2, size_t n);
 
 #endif
