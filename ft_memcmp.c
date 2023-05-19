7905b0485a88371d0d0ed2f78ef3fdfeed7000f5
shmorish <01morishi@gmail.com>
2023-05-19 20:52:18 +0900

    fix memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: shmorish <shmorish@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 21:43:23 by morishitash       #+#    #+#             */
-/*   Updated: 2023/05/19 20:41:40 by shmorish         ###   ########.fr       */
+/*   Updated: 2023/05/19 20:51:28 by shmorish         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,16 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	unsigned char	*buf1;
 	unsigned char	*buf2;
+	size_t			i;
 
+	i = 0;
 	buf1 = (unsigned char *)s1;
 	buf2 = (unsigned char *)s2;
-	while (n != 0)
+	while (i < n)
 	{
-		if (buf1 != buf2)
-			return (buf2 - buf1);
-		buf1++;
-		buf2++;
-		n--;
+		if (buf1[i] != buf2[i])
+			return (buf1[i] - buf2[i]);
+		i++;
 	}
 	return (0);
 }
