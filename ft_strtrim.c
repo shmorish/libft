b349851244a6b7e7e291bf84f3bc0cffb108aca5
Morishita Shoto <morishita.shoto@classmethod.jp>
2023-05-21 04:44:55 +0900

    done calloc

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -1,26 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: morishitashoto <morishitashoto@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/20 02:25:06 by morishitash       #+#    #+#             */
-/*   Updated: 2023/05/21 04:44:04 by morishitash      ###   ########.fr       */
+/*   Created: 2023/05/21 04:10:05 by morishitash       #+#    #+#             */
+/*   Updated: 2023/05/21 04:11:52 by morishitash      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-
-void	*ft_calloc(size_t count, size_t size)
-{
-	void	*ptr;
-
-    if (size != 0 && count > SIZE_MAX / size)
-        return (NULL);
-    ptr = malloc(count * size);
-    if (ptr == NULL)
-        return (ptr);
-	ft_bzero(ptr, size * count);
-	return (ptr);
-}
+char    *ft_strtrim(char const *s1, char const *set);
