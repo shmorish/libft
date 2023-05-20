3c4fd8c37c3dcce1ae40e80af0656f1451ef7d69
Morishita Shoto <morishita.shoto@classmethod.jp>
2023-05-20 12:00:54 +0900

    finish strdup

--- ft_substr.c
+++ ft_substr.c
@@ -1,33 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: morishitashoto <morishitashoto@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/20 02:29:35 by morishitash       #+#    #+#             */
-/*   Updated: 2023/05/20 11:52:30 by morishitash      ###   ########.fr       */
+/*   Created: 2023/05/20 11:59:45 by morishitash       #+#    #+#             */
+/*   Updated: 2023/05/20 12:00:16 by morishitash      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include "libft.h"
-
-char	*ft_strdup(const char *s1)
-{
-	int		i;
-	int		len;
-	char	*ptr;
-
-	i = 0;
-	len = ft_strlen(s1);
-	if ((ptr = (char*)malloc(sizeof(char) * (len + 1))) == NULL)
-		return (NULL);
-	while (i < len)
-	{
-		ptr[i] = s1[i];
-		i++;
-	}
-	ptr[i] = '\0';
-	return (ptr);
-}
 
