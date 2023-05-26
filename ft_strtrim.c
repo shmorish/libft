ef2a5cc508d11f0af6cd58f7c186710cb269ad03
shmorish <01morishi@gmail.com>
2023-05-26 22:02:05 +0900

    done trim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -3,11 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: morishitashoto <morishitashoto@student.    +#+  +:+       +#+        */
+/*   By: shmorish <shmorish@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/21 04:10:05 by morishitash       #+#    #+#             */
-/*   Updated: 2023/05/21 04:11:52 by morishitash      ###   ########.fr       */
+/*   Updated: 2023/05/26 21:55:43 by shmorish         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char    *ft_strtrim(char const *s1, char const *set);
+#include "libft.h"
+
+char	*ft_strtrim(const char *s1, const char *set)
+{
+	size_t	start;
+	size_t	end;
+
+	start = 0;
+	end = ft_strlen(s1) - 1;
+	while (ft_strchr(set, s1[start]) != NULL)
+	{
+		start++;
+	}
+	while (ft_strrchr(set, s1[end]) != NULL)
+	{
+		end--;
+	}
+	return (ft_substr(s1, start, end - start + 1));
+}
+
+// #include <stdio.h>
+
+// int	main(void)
+// {
+// 	char s[] = "   a a a 3127890 a aaa  ";
+// 	printf("%s\n", ft_strtrim(s, " a"));
+// }
