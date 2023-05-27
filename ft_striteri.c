39a0caa993bb8f5c7da589ebac4c8d61372a34e5
shmorish <01morishi@gmail.com>
2023-05-28 05:15:17 +0900

    done striteri with tester

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: shmorish <shmorish@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/28 05:05:25 by shmorish          #+#    #+#             */
-/*   Updated: 2023/05/28 05:06:13 by shmorish         ###   ########.fr       */
+/*   Updated: 2023/05/28 05:13:07 by shmorish         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,27 @@
 
 void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
-	
+	size_t	idx;
+
+	if (s == NULL || f == NULL)
+		return ;
+	idx = 0;
+	while (*s != '\0')
+		f(idx++, s++);
 }
+
+// #include <string.h>
+// #include <stdio.h>
+
+// void f(unsigned int i, char *s)
+// {
+// 	(void)i;
+// 	*s = ft_tolower(*s);
+// }
+
+// int	main(void)
+// {
+// 	char s[] = "aAaAa";
+// 	ft_striteri(s, f);
+// 	printf("%s\n", s);
+// }
