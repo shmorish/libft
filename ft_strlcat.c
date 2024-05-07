6ab255b19c3f348aab22e90de13a42279b03b435
shmorish <01morishi@gmail.com>
2024-05-07 20:31:01 +0900

    fix strlcat strlcpy

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -3,28 +3,43 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: morishitashoto <morishitashoto@student.    +#+  +:+       +#+        */
+/*   By: shmorish <shmorish@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 19:35:49 by shmorish          #+#    #+#             */
-/*   Updated: 2023/06/08 14:47:25 by morishitash      ###   ########.fr       */
+/*   Updated: 2024/05/07 20:25:51 by shmorish         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+size_t	ft_strnlen(const char *s, size_t maxlen)
 {
-	size_t	dest_len;
-	size_t	src_len;
+	size_t	len;
 
-	src_len = ft_strlen(src);
-	if (dstsize == 0)
-		return (src_len);
-	dest_len = ft_strlen(dst);
-	if (dstsize <= dest_len)
-		return (dstsize + src_len);
-	ft_strlcpy(&dst[dest_len], src, dstsize - dest_len);
-	if (dstsize > dest_len)
-		dstsize = dest_len;
-	return (src_len + dstsize);
+	len = 0;
+	while (len < maxlen)
+	{
+		if (!*s)
+			break ;
+		len++;
+		s++;
+	}
+	return (len);
+}
+
+size_t	ft_strlcat(char *dst, const char *src, size_t maxlen)
+{
+	const size_t	srclen = ft_strlen(src);
+	const size_t	dstlen = ft_strnlen(dst, maxlen);
+
+	if (dstlen == maxlen)
+		return (maxlen + srclen);
+	if (srclen < maxlen - dstlen)
+		ft_memcpy(dst + dstlen, src, srclen + 1);
+	else
+	{
+		ft_memcpy(dst + dstlen, src, maxlen - dstlen - 1);
+		dst[maxlen - 1] = '\0';
+	}
+	return (dstlen + srclen);
 }
--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -3,27 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: morishitashoto <morishitashoto@student.    +#+  +:+       +#+        */
+/*   By: shmorish <shmorish@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 19:33:01 by shmorish          #+#    #+#             */
-/*   Updated: 2023/06/04 22:17:37 by morishitash      ###   ########.fr       */
+/*   Updated: 2024/05/07 20:30:35 by shmorish         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+size_t	ft_strlcpy(char *dst, const char *src, size_t maxlen)
 {
-	size_t	i;
+	const size_t	srclen = ft_strlen(src);
 
-	if (dstsize == 0)
-		return (ft_strlen(src));
-	i = 0;
-	while (src[i] && i < (dstsize - 1))
+	if (srclen + 1 < maxlen)
+		ft_memcpy(dst, src, srclen + 1);
+	else if (maxlen != 0)
 	{
-		dst[i] = src[i];
-		i++;
+		ft_memcpy(dst, src, maxlen - 1);
+		dst[maxlen - 1] = '\0';
 	}
-	dst[i] = '\0';
-	return (ft_strlen(src));
+	return (srclen);
 }
