2ec5617880e0e7d20c970aee900a3e385c2a0bdd
shmorish <110565242+shmorish@users.noreply.github.com>
2025-05-24 13:34:09 +0900

    Update ft_lstadd_back.c

--- srcs/list/ft_lstadd_back.c
+++ srcs/list/ft_lstadd_back.c
@@ -14,18 +14,15 @@
 
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
-	t_list	*output;
+	t_list	*last;
 
-	if (new == NULL)
+	if (new == NULL || lst == NULL)
 		return ;
-	if (lst != NULL)
+	if (*lst == NULL)
 	{
-		if (*lst == NULL)
-			*lst = new;
-		else
-		{
-			output = ft_lstlast(*lst);
-			output->next = new;
-		}
+		*lst = new;
+		return ;
 	}
+	last = ft_lstlast(*lst);
+	last->next = new;
 }
